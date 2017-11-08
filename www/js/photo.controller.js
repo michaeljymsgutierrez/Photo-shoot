c3b3ffb919eb9d09eb89b8af70d77965e2d571ed
Chaelgutierrez <michael@vielsoft.com>
2017-11-08 16:11:52 +0800

    Added camera load on platform ready

--- www/js/photo.controller.js
+++ www/js/photo.controller.js
@@ -3,6 +3,33 @@
 /*
     Photoshoot Controller
 */
-app.controller('photoCtrl', ['$scope', '$ionicPlatform', function($scope, $ionicPlatform) {
+app.controller('photoCtrl', ['$scope', '$ionicPlatform', '$cordovaCamera', function($scope, $ionicPlatform, $cordovaCamera) {
+
+    /*
+        Platform Ready
+    */
+    $ionicPlatform.ready(function() {
+        var options = {
+            quality: 50,
+            destinationType: 0,
+            sourceType: 1,
+            allowEdit: true,
+            encodingType: 1,
+            targetWidth: 100,
+            targetHeight: 100,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false,
+            correctOrientation: true
+        };
+
+        /*
+            Get Picture
+        */
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            console.log(imageData)
+        }, function(err) {
+            console.log(err);
+        });
+    });
 
 }]);
