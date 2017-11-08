b4a9bdc25fb53c53bf0420d20dc0238b6762ed3f
Chaelgutierrez <michael@vielsoft.com>
2017-11-08 11:25:16 +0800

    Included js files, initialized db

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,13 @@
-var app = angular.module('starter', ['ionic', 'ionic.native'])
+'use strict';
 
-app.run(function($ionicPlatform) {
+/*
+  Photoshoot module
+*/
+var app = angular.module('photoshoot', ['ionic', 'ionic.native']);
+var db = null;
+
+
+app.run(function($ionicPlatform, DBAccess) {
     $ionicPlatform.ready(function() {
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -9,5 +16,13 @@ app.run(function($ionicPlatform) {
         if (window.StatusBar) {
             StatusBar.styleDefault();
         }
+        db = window.sqlitePlugin.openDatabase({ name: 'photoshoot.db', location: 'default' });
+
+        DBAccess.execute("CREATE TABLE IF NOT EXISTS photo (id integer primary key, filename longtext, image longtext, created datetime)", []).then(function(res) {
+            console.log(res);
+        }, function(err) {
+            console.log(err);
+        });
+
     });
 });
